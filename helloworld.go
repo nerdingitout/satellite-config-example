6de7bedb24abeefb8092d3fcde9d07a3f7160092
Su <su@New-su.fritz.box>
2023-02-13 17:36:15 +0100

    Stage branch

--- helloworld.go
+++ helloworld.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World - Prod Env!")
+		fmt.Fprintf(w, "Hello World - Stage Env!")
 	})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
